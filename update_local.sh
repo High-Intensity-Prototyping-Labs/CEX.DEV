8aca38e540d2abd0c4ad95e5533cf2b97125a071
amellalalex <github@alexamellal.com>
2024-01-20 13:10:20 +0100

    Updated to (v)

---
+++ update_local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the newest version from user 
+read -p "Version number (2.3.0): " version 
+
+# Do the thing 
+cp ~/Documents/CEX/CEX\ –\ The\ Standard\ v$(version).pdf downloads
+cp ~/Documents/Obsidian/Main/HIPL/CEX\ –\ The\ Standard\ v$(version).md readme.md
+vim index.html
+git add .
+git commit -m "Updated to (v$(version))"
+git push
+
+ssh linode 'cd /var/www/cex && git pull'
